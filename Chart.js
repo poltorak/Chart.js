c35cd31032d9c21d39043261761378c8e99975c5
Szymon Półtorak <szymon.poltorak@subcom.me>
2015-04-10 11:36:44 +0200

    optimize code

--- Chart.js
+++ Chart.js
@@ -1222,12 +1222,9 @@
 
 			ctx.beginPath();
 
-			this.outerRadius = Math.max(0, this.outerRadius);
-			this.innerRadius = Math.max(0, this.innerRadius);
+			ctx.arc(this.x, this.y, Math.max(0, this.outerRadius), this.startAngle, this.endAngle);
 
-			ctx.arc(this.x, this.y, this.outerRadius, this.startAngle, this.endAngle);
-
-			ctx.arc(this.x, this.y, this.innerRadius, this.endAngle, this.startAngle, true);
+			ctx.arc(this.x, this.y, Math.max(0, this.innerRadius), this.endAngle, this.startAngle, true);
 
 			ctx.closePath();
 			ctx.strokeStyle = this.strokeColor;
